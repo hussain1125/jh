121e39c5c6ce75345101d874a6a46c877a0bc266
Hussain Raza <153796123+hussain1125@users.noreply.github.com>
2024-11-30 21:32:52 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.html"], // This includes all HTML files in the dist folder and subfolders
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
